1fd2ed4e7506c66e0fbc0cc6c2266b72b5a6455d
bashman100 <bashman2000@ymail.com>
2016-10-13 14:40:46 -0600

    Commit v0.1.2 heroku compatability

--- eventio/settings.py
+++ eventio/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/assets/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'assets'),   
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
